446adee2e65997d4e065eec355b9ca9eda59dad2
reneesaks <renee.saks@khk.ee>
2017-01-12 12:55:02 +0200

    created html and template class

---
+++ project/classes/template.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Renee
+ * Date: 1/12/2017
+ * Time: 12:27
+ */
+// if TMPL_DIR is not defined
+if(!defined('TMPL_DIR')) {
+    // define this constant and use it in class template
+    define('TMPL_DIR', '../tmpl/');
+}
+class template
+{
+    // class variables
+    var $file = ''; // template file name
+    var $content = false; // template content - now is empty
+
+    // class methods
+    function loadFile() {
+        $f = $this->file; // use file name variable
+        // if problems occur with tmpl directory
+        if(!is_dir(TMPL_DIR)) {
+            echo 'Directory '.TMPL_DIR.' not found.<br/>';
+            exit;
+        }
+        // if we are in tmpl directory, $this->file is 'tmpl/file.html'
+        if(file_exists($f) and is_file($f) and is_readable($f)) {
+            $this->readFile($f);
+        }
+        // set path to template file: tmpl/file.html, $this->file is 'file.html'
+        $f = TMPL_DIR.$this->file;
+        if(file_exists($f) and is_file($f) and is_readable($f)) {
+            $this->readFile($f);
+        }
+        // set only file name, $this->file is 'file'
+        $f = TMPL_DIR.$this->file.'.html';
+        if(file_exists($f) and is_file($f) and is_readable($f)) {
+            $this->readFile($f);
+        }
+        if($this->content === false) {
+            echo 'Could not read file '.$this->file.'.<br/>';
+            exit;
+        }
+    }
+
+    function readFile($f) {
+        $this->content = file_get_contents($f);
+    }
+}

0be1757271e03baf853e771f30b5639d917db2ce
reneesaks <renee.saks@khk.ee>
2017-01-18 10:47:19 +0200

    created menu objects

--- project/menu.php
+++ project/menu.php
@@ -8,4 +8,5 @@
 // menu.php - create page menu
 // create menu template objects
 // for menu and menu items
-$menu = new template('menu.menu');
+$menu = new template('menu.menu'); // menu directory is menu/menu.html
+$item = new template('menu.item');

0e71a130eee78413e813d99f991a40167794760f
reneesaks <renee.saks@khk.ee>
2017-01-18 09:15:01 +0200

    create linkobject variables and constructor

--- project/classes/linkobject.php
+++ project/classes/linkobject.php
@@ -11,5 +11,17 @@ require_once 'http.php';
 // only for testing
 class linkobject extends http
 {
+    // class variables
+    var $baseUrl = false; // base url
+    var $protocol = 'http://'; // protocol for url http
+    var $delim = '&amp;'; // & html tag name1=value1&name2=value2
+    var $eq = '='; // = for url element pair element_name = element_value
 
+    // class methods
+    // construct
+    // create base url: http://XXX.XXX.XXX.XXX/path_to_file.php
+    function __construct() {
+        parent::__construct();
+        $this->baseUrl = $this->protocol.HTTP_HOST.SCRIPT_NAME;
+    }
 }
--- project/index.php
+++ project/index.php
@@ -39,24 +39,6 @@ require_once CLASSES_DIR.'http.php';
 // create and output http object
 $http = new http();
 // control http object output
-echo '<pre>';
-print_r($http);
-echo '</pre>';
-// control http constants
-echo REMOTE_ADDR.'<br/>';
-echo PHP_SELF.'<br/>';
-echo SCRIPT_NAME.'<br/>';
-echo HTTP_HOST.'<br/>';
-echo '<hr/>';
-// create http data pairs and set up into $http-> vars array
-$http->set('Kasutaja', 'Renee');
-$http->set('Tund', 'Programmeerimisvahendid');
-// control $http-> vars object output
-echo '<pre>';
-print_r($http->vars);
-echo '</pre>';
-//
-// linkobject class testing
 // import linkobject class file
 require_once CLASSES_DIR.'linkobject.php';
 // create linkobject type object
@@ -64,5 +46,18 @@ $linkobject = new linkobject();
 // control linkobject output
 echo '<pre>';
 print_r($linkobject);
+echo '</pre>';
+// control http constants
+echo REMOTE_ADDR.'<br />';
+echo PHP_SELF.'<br />';
+echo SCRIPT_NAME.'<br />';
+echo HTTP_HOST.'<br />';
+echo '<hr />';
+// create http data pairs and set up into $http->vars array
+$http->set('kasutaja', 'Anna');
+$http->set('tund', 'php programmeerimisvahendid');
+// control $http->vars object output
+echo '<pre>';
+print_r($http->vars);
 echo '</pre>';
 ?>

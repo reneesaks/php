8b00d0cc8bca537f585b52f4eafaddf2119637ba
reneesaks <renee.saks@khk.ee>
2017-01-18 11:08:02 +0200

    change and check menu item element

--- project/menu.php
+++ project/menu.php
@@ -12,9 +12,13 @@
 $menu = new template('menu.menu'); // menu directory is menu/menu.html
 $item = new template('menu.item');
 
+// add pairs of item template element names and real values
+$item->set('name', 'Esimene leht');
+$link = $http->getLink(array('page' => 'first'));
+$item->set('link', $link);
+
+// control created item output
 echo '<pre>';
-print_r($menu);
 print_r($item);
 echo '</pre>';
-
 ?>

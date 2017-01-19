ecba2537718ba3c24a33b0e755ae0bf98e441b11
reneesaks <renee.saks@khk.ee>
2017-01-19 12:17:31 +0200

    removed unwanted lines

--- project/acts/default.php
+++ project/acts/default.php
@@ -5,4 +5,4 @@
  * Date: 1/18/2017
  * Time: 15:13
  */
-echo 'default fail sisu';
+echo 'default faili sisu';
--- project/index.php
+++ project/index.php
@@ -34,40 +34,8 @@ $tmpl->set('menu', $menu->parse());
 $tmpl->set('nav_bar', 'minu navigatsioon');
 $tmpl->set('lang_bar', 'minu keeleriba');
 $tmpl->set('content', 'minu sisu');
-/*
-// control the content of template object
-echo '<pre>';
-print_r($tmpl);
-echo '</pre>';
-*/
 // output template content set up with real values
 echo $tmpl->parse();
-// control http object output
-/*echo '<pre>';
-print_r($http);
-echo '</pre>';*/
-// control http constants
-echo REMOTE_ADDR.'<br />';
-echo PHP_SELF.'<br />';
-echo SCRIPT_NAME.'<br />';
-echo HTTP_HOST.'<br />';
-echo '<hr />';
-// create http data pairs and set up into $http->vars array
-$http->set('kasutaja', 'Renee');
-$http->set('tund', 'php programmeerimisvahendid');
-// control $http->vars object output
-/*echo '<pre>';
-print_r($http->vars);
-echo '</pre>';*/
-// control link creation
-$link = $http->getLink(array('kasutaja'=>'anna', 'parool'=>'qwerty'));
-/*// control http output
-echo '<pre>';
-print_r($http);
-echo '</pre>';
-// control element value by name
-echo $http->get('act');*/
-//
 // import actions and check em
 require_once 'act.php';
 ?>
